2b09df6732f460a16e6f015822370fffdcc570b8
barrydevp <barrydevp@gmail.com>
2020-05-23 22:08:03 +0700

    update cors allow header

--- src/common/utils/cors.go
+++ src/common/utils/cors.go
@@ -6,18 +6,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func _generateAndAppendURL(listURL *[]string, uri string) {
+	*listURL = append(*listURL, "http://"+uri)
+	*listURL = append(*listURL, "http://www."+uri)
+	*listURL = append(*listURL, "https://"+uri)
+	*listURL = append(*listURL, "https://www."+uri)
+}
+
 func Cors() gin.HandlerFunc {
 	_corsConfig := cors.DefaultConfig()
 
 	allowOrigins := []string{"http://localhost:3000"}
-	allowOrigins = append(allowOrigins, "http://" + constants.WEB_HOST)
-	allowOrigins = append(allowOrigins, "https://" + constants.WEB_HOST)
+	_generateAndAppendURL(&allowOrigins, constants.WEB_HOST)
+	_generateAndAppendURL(&allowOrigins, "test-cors.org")
+	// allowOrigins = append(allowOrigins, "http://"+constants.WEB_HOST)
+	// allowOrigins = append(allowOrigins, "https://.test-cors.org")
+	// allowOrigins = append(allowOrigins, "http://.test-cors.org")
+	// allowOrigins = append(allowOrigins, "https://www.test-cors.org")
+	// allowOrigins = append(allowOrigins, "http://www.test-cors.org")
+	// allowOrigins = append(allowOrigins, "https://"+constants.WEB_HOST)
+
+	allowHeaders := []string{"Authorization"}
 
 	_corsConfig.AllowOrigins = allowOrigins
 	_corsConfig.AllowCredentials = true
+	_corsConfig.AllowHeaders = allowHeaders
+	_corsConfig.AllowFiles = true
 	_cors := cors.New(_corsConfig)
 
 	return _cors
 }
-
-
